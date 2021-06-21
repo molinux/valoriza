46c036a55a2a2db08171db2f86fa21ecea0e0574
Molinux <cybercrowbr@gmail.com>
2021-06-20 22:29:52 -0300

    feat(nodejs): :tada: In the beginning God create the code
    In the beginning God create the code. Now the repository was formless and empty, darkness was over
    the surface of the deep

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/test', (request,response) => {
+  return response.send("Olá NLW");
+});
+
+app.post('/test-post', (request, response) => {
+  response.send("Olá NLW, método POST");
+})
+
+app.listen(3000, () => console.log("Server is running!"));
