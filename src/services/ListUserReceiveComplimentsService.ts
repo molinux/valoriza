484e90ec51a14f74cd6f8f7d048a8fbf523c7168
Molinux <cybercrowbr@gmail.com>
2021-07-26 11:58:30 -0300

    style(eslint): :art: Commit before eslint
    Commit to prevent some shit with eslint implementation

---
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories";
+import { UsersRepositories } from "../repositories/UsersRepository";
+
+class ListUserReceiveComplimentsService {
+  async execute(user_id: string) {
+    const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+    const compliments = await complimentsRepositories.find({
+      where: {
+        user_receiver: user_id
+      },
+    });
+
+    return compliments
+  }
+
+}
+
+export { ListUserReceiveComplimentsService }
